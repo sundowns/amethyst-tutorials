01dd1278f14e5c18366bff414faa5e1d5872c74b
sundowns <tom@smallridge.com.au>
2019-03-02 18:56:35 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate amethyst;
+use amethyst::prelude::*;
+use amethyst::renderer::{
+    DisplayConfig, DrawFlat2D, Event, Pipeline, RenderBundle, Stage, VirtualKeyCode,
+};
+use amethyst::utils::application_root_dir;
+
+pub struct Pong;
+
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+    let config = DisplayConfig::load(&path);
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new()),
+    );
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?;
+
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
